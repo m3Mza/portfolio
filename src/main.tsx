7a688f36653dafcaf9368dd26d9b986c2fcc701b
m3Mza <mimamirkop@gmail.com>
2025-12-24 17:46:59 +0100

    sdasdfasfgasf

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Work from './Work.tsx'
 import About from './About.tsx'
 
+function MainRoutes() {
+  const location = useLocation();
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<App />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <MainRoutes />
     </BrowserRouter>
   </StrictMode>,
 )
